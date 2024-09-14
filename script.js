9eeec1f2b3b4e13b1ac5e1442003ae2797b73768
awint00r <austinwinterberg@yahoo.com>
2024-09-14 16:48:52 -0500

    Added an if/else statement if the user guesses the correct amount; otherwise decreases the guesses by 1.

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let guessesRemaining = 3;
 /* Declares guess as the value entered by the user. */
 let guess = prompt("Guess a Number between 1 - 50.");
 
+/* If the user guesses the correct number, an alert will display letting them know they got it and what the correct number was; otherwise the guesses remaining will decrease by 1. */
+if (guess == (randomNumber)) {
+    alert('You NAILED it! The correct number was ' + randomNumber)
+} else {
+    guessesRemaining--;
+};
