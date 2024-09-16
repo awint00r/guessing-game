c91851796e515bcabaa91c21e56de679b59da5cb
awint00r <austinwinterberg@yahoo.com>
2024-09-16 15:37:33 -0500

    Added additional content within if/else statements to make all alerts produce valid results. Additionally, added automatic reload to end of the JS file to reload the page once the game is over.

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ console.log("First Guess:", guess)
 
 /* If the user guesses the correct number, an alert will display letting them know they got it and what the correct number was; otherwise the guesses remaining will decrease by 1 and the user will be alerted on whether they were too high or too low.. */
 if (guessesRemaining == 3) {
-    if (guess < (randomNumber)) {
+    if (guess < (randomNumber) && guess > 0) {
         guessesRemaining--;
         alert('That number is too low ' + userName + '. You have ' + guessesRemaining + ' guesses remaining.');
     } else if (guess == (randomNumber)) {
         alert('You NAILED it ' + userName + '! The correct number was ' + randomNumber + '.')
         console.log(guessesRemaining)
-    } else if (guess > (randomNumber)) {
+    } else if (guess > (randomNumber) && guess < 50) {
         guessesRemaining--;
         alert('That number is too high ' + userName + '. You have ' + guessesRemaining + ' guesses remaining');
-    } else if (guess != (Number)) {
+    } else if (guess != (Number) || guess < 1 || guess > 50) {
         alert('Please enter a number, using digits, between 1-50 and try again.');
         guess = Number(prompt(" Hi there " + userName + ". Guess a number, using digits, between 1 - 50."));
         console.log("Second Guess:", guess)
@@ -42,15 +42,15 @@ if (guessesRemaining == 3) {
 
 if (guessesRemaining == 2) {
     guess = prompt("Guess a number, using digits, between 1 - 50.");
-    if (guess < (randomNumber)) {
+    if (guess < (randomNumber) && guess > 0) {
         guessesRemaining--;
         alert('That number is too low ' + userName + '. You have ' + guessesRemaining + ' guesses remaining.');
     } else if (guess == (randomNumber)) {
         alert('You NAILED it ' + userName + '! The correct number was ' + randomNumber + '.')
-    } else if (guess > (randomNumber)) {
+    } else if (guess > (randomNumber) && guess < 50) {
         guessesRemaining--;
         alert('That number is too high ' + userName + '. You have ' + guessesRemaining + ' guesses remaining');
-    } else if (guess != (Number)) {
+    } else if (guess != (Number) || guess < 1 || guess > 50) {
         alert('Please enter a number, using digits, between 1-50 and try again.');
         guess = Number(prompt(" Hi there " + userName + ". Guess a number, using digits, between 1 - 50."));
         console.log("Second Guess:", guess)
@@ -70,9 +70,11 @@ if (guessesRemaining == 1) {
     guess = prompt("Guess a number, using digits, between 1 - 50.");
     if (guess == (randomNumber)) {
         alert('You NAILED it ' + userName + '! The correct number was ' + randomNumber + '.')
-    } else if (guess == (randomNumber)) {
-        alert('You NAILED it ' + userName + '! The correct number was ' + randomNumber + '.')
     } else if (guess !== (randomNumber)) {
-        alert('Sorry ' + userName + ', you did not guess the correct value. The correct number was ' + randomNumber + '. Please refresh the page to play again.');
+        alert('Sorry ' + userName + ', you did not guess the correct value. The correct number was ' + randomNumber + '. After pressing OK, the game will start over 5 seconds later.');
     }
 }
+/* Makes the page reload after 5 seconds and effectively starts the game over */
+setTimeout(function(){
+    window.location.reload();
+ }, 5000);
